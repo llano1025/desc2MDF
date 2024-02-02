6152555a3a5be7e4f515a8c12a2d08f7ae247a18
llano1025 <hwf1025@hotmail.com>
2024-02-02 15:10:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,27 @@
 from data import data_loader as dl
 from exc.exc import BertClassifier
-
+import os
 
 # Use environment
-# device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-# proxy = 'http://18366:@N37f343734@!@proxy.emsd.hksarg:80'
-# os.environ['http_proxy'] = proxy
-# os.environ['HTTP_PROXY'] = proxy
-# os.environ['https_proxy'] = proxy
-# os.environ['HTTPS_PROXY'] = proxy
-# os.environ['CURL_CA_BUNDLE'] = ''
+proxy = ''
+os.environ['http_proxy'] = proxy
+os.environ['HTTP_PROXY'] = proxy
+os.environ['https_proxy'] = proxy
+os.environ['HTTPS_PROXY'] = proxy
+os.environ['CURL_CA_BUNDLE'] = ''
 
 model_name = 'bert-base-uncased'
-bert_path = 'G:/huggingface/hub'
-model_type = "Desc2MDF"
-data = dl.Dataloader_Desc2MDF(model_name, bert_path)
-model = BertClassifier(model_name, model_type, data)
-# model._train(data.dataloader_train, data.dataloader_validation)
+bert_path = ''
+model_type = "MDF2Rec"  # MDF2Rec / Desc2MDF
 
+# data = dl.Dataloader_Desc2MDF(model_name, bert_path)
+data = dl.Dataloader_MDF2Rec(model_name, bert_path)
+model = BertClassifier(model_name, model_type, data)
+model._train(data.dataloader_train, data.dataloader_validation)
 
 file_path = "data/beseformDB.mdfAssetDescriptions.xlsx"
-pred_data = data.predict_data(file_path)
-prediction = model._predict(pred_data)
+dataloader_predict = data.predict_data(file_path)
+prediction = model._predict(dataloader_predict)
+print(prediction)
 
-print('hello world')
+print('Hello World')
