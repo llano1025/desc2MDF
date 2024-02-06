f945df32de2a07b19d94142f14353fa1ba9b07f8
Linus Chan <ylchan@emsd.gov.hk>
2024-02-06 17:23:34 +0800

    Support text input instead csv input

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 This project use AI to turn description to MDF group.
 
-### First time start the project
+## First time start the project
 
 1.  Create virtual env. The 1st venv is option. The 2nd venv is we named the virtual env as ‘venv’
     > python -m venv venv
@@ -10,7 +10,7 @@ This project use AI to turn description to MDF group.
     > pip install -r requirements.txt
 4.  Python run main.py
 
-### Other time start the project
+## Everytime start the project
 
 1. Active the virtual venv by gitbash
    > source venv/Scripts/activate
@@ -18,12 +18,26 @@ This project use AI to turn description to MDF group.
    > pip install -r requirements.txt
 3. Python run main.py
 
-### Input
+## Input
 
-- Input data: data\beseformDB.mdfAssetDescriptions.xlsx
+#### MDF2Rec (Desc2Rec?)
+
 - Training data: data\BasicInfo_Result.xlsx
+- Input data: data\beseformDB.mdfAssetDescriptions.xlsx
+
+#### Desc2MDF
+
+- Training data: data\recForm.xlsx
+- Input data: data\beseformDB.mdfAssetDescriptions.xlsx
+
 - No. of epochs: self.epochs = 50 (default)
 
-### Output
+## Output
+
+#### MDF2Rec (Desc2Rec?)
 
 - Output data: exc/outputs/MDF2Rec_output.xlsx
+
+#### Desc2MDF
+
+- Output data: exc/outputs/Desc2MDF_output.xlsx
--- main.py
+++ main.py
@@ -10,20 +10,27 @@ os.environ['https_proxy'] = proxy
 os.environ['HTTPS_PROXY'] = proxy
 os.environ['CURL_CA_BUNDLE'] = ''
 
+MODEL_TYPE = "Desc2MDF" # MDF2Rec / Desc2MDF
+IS_TRAIN_MODEL_NEEDED = False
+IS_INPUT_BY_CSV = False
+INPUT_BY_TEXT = ['INFUSION PUMPS, LONGG', 'WATER PURIFICATION']
+
 model_name = 'bert-base-uncased'
 bert_path = ''
 
-model_type = "MDF2Rec"  # MDF2Rec / Desc2MDF
-data = dl.Dataloader_MDF2Rec(model_name, bert_path)
+match (MODEL_TYPE):
+    case ("MDF2Rec"):
+        data = dl.Dataloader_MDF2Rec(model_name, bert_path)
+    case ("Desc2MDF"):
+        data = dl.Dataloader_Desc2MDF(model_name, bert_path)
 
-# model_type = "Desc2MDF"
-# data = dl.Dataloader_Desc2MDF(model_name, bert_path)
+model = BertClassifier(model_name, MODEL_TYPE, data)
 
-model = BertClassifier(model_name, model_type, data)
-model._train(data.dataloader_train, data.dataloader_validation)
+if(IS_TRAIN_MODEL_NEEDED and IS_INPUT_BY_CSV):
+    model._train(data.dataloader_train, data.dataloader_validation)
 
 file_path = "data/beseformDB.mdfAssetDescriptions.xlsx"
-dataloader_predict, predict_ids = data.predict_data(file_path)
+dataloader_predict, predict_ids = data.predict_data(file_path, IS_INPUT_BY_CSV, INPUT_BY_TEXT)
 prediction = model._predict(dataloader_predict, predict_ids)
 print(prediction)
 
